373be23bc27b34a54408bda4ed3bbec665c48377
Wing Fung Wu <44103783+WingFungWu@users.noreply.github.com>
2023-03-06 22:58:11 -0800

    save

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 from nltk.chat.util import Chat, reflections
 
 pairs = [
-        ['yo|hi|hey|hello|hola|what\'s good', ['Nice to meet you!', 'Hi there!', 'How can I help you?']],
-        ['what\'s your name|what is your name?|what are you?|who are you?|your name please?|your name?', ['Hi I\'m Chat 4 8 2']],
+        ['yo|hi|hey|hello|what\'s good', ['Nice to meet you!', 'Hi there!', 'How can I help you?']],
+        ['what\'s your name|what is your name?|what are you?|who are you?|your name please?|your name?', ['Hi, I\'m Chat 4 82', 'Call me Chat 4 82', 'I\'m Chat 4 82, nice to meet you']],
         ['thanks|thank you|that\'s helpful|awesome, thanks|thanks for helping me', ['happy to help!', 'anytime', 'no problem']],
         ['.*best soccer player.*', ['Lionel Messi', 'Cristiano Ronaldo', 'Robert Lewandowski', 'Kevin De Bruyne', 'Kylian Mbappe', 'Neymar', 'Erling Haaland']],
         ['.*fastest animal.*', ['The fastest land animal is the cheetah, which can reach speeds of up to 76 mph', 'The fastest bird, and the fastest member of the animal kingdom, is the peregrine falcon, which can dive at speeds of up to 242 mph', 'The fastest water-based animal is the black marlin, which can swim at speeds of up to 82 mph']],
