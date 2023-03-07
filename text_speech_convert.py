eb9f08a481a0df81c3fe030b14150b49b9b53184
Wing Fung Wu <44103783+WingFungWu@users.noreply.github.com>
2023-03-06 22:49:22 -0800

    save

--- text_speech_convert.py
+++ text_speech_convert.py
@@ -3,9 +3,7 @@ import logging
 
 from aiy.cloudspeech import CloudSpeechClient
 from aiy.voice.tts import say
-import sys
-sys.path.insert(1, '/home/pi/csc482_lab4/CSChatBot-main')
-import chatbot, sql_queries
+from chatbot import chat
 
 def main():
     language, _ = locale.getdefaultlocale()
@@ -14,23 +12,18 @@ def main():
     client = CloudSpeechClient()
     while True:
         logging.info('Say something.')
-        text = client.recognize(language_code=language)
-        if text is None:
+        q = client.recognize(language_code=language)
+        if q is None:
             logging.info('You said nothing.')
             continue
 
-        logging.info('You said: "%s"' % text)
-        text = text.lower()
-        if 'goodbye' in text:
+        logging.info('You said: "%s"' % q)
+        q = q.lower()
+        if 'goodbye' in q:
             break
         else:
-            bot = chatbot.ChatBot()
-            print("Hello I am EKK, your Cal Poly Virtual Assistant. How can I help you today?")
-            q = text
-            entities, answer = bot.get_sample_answers(q)
-            if answer != -1:
-                query = sql_queries.Query(q, entities, answer)
-                query.queryDB()
+            a = chat.respond(q)
+            say(a)
             
 
 if __name__ == '__main__':
