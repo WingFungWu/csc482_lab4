6c2e90996450d58fe1eab433fc19a71f8dead27c
creavill <63218370+creavill@users.noreply.github.com>
2023-03-09 11:40:50 -0800

    list work almost done

---
+++ list.py
@@ -0,0 +1,72 @@
+import os
+
+def createList(lstName):
+    try:
+        open(lstName+".txt","x")
+        return "List " + lstName + " has been created"
+    except:
+        return "List " + lstName + " already exists"
+
+def addToList(lstName,item):
+    if not checkIfFileExists(lstName):
+        return "List " + lstName + " does not exist"
+    f = open(lstName + ".txt", "a") 
+    f.write(item+"\n")
+    return item + " added to " + lstName
+
+def removeFromList(lstName,item):
+    if not checkIfFileExists(lstName):
+        return "List " + lstName + " does not exist"
+    f = open(lstName+ ".txt", "r")
+    lines = f.readlines()
+    f.close()
+    w= open(lstName+ ".txt", "w")
+    removed=False
+    for l in lines:
+        if l != item+"\n":
+            w.write(l)
+        else:
+            removed=True
+    if removed:
+        return item + " has been removed from " + lstName
+    return item + " was not in " + lstName
+
+def deleteList(lstName):
+    if not checkIfFileExists(lstName):
+        return "List " + lstName + " does not exist"
+    os.remove(lstName +".txt")
+    return lstName + " has been deleted"
+
+def readList(lstName):
+    if not checkIfFileExists(lstName):
+        return "List " + lstName + " does not exist"
+    f = open(lstName + ".txt", "r")
+    a = f.readlines()
+    if len(a) ==0:
+        return "There is nothing in " + lstName
+    else:
+        return ''.join([str(item) for item in a])
+
+def clearList(lstName):
+    if not checkIfFileExists(lstName):
+        return "List " + lstName + " does not exist"
+    deleteList(lstName)
+    createList(lstName)
+    return "List " + lstName + " has been cleared" 
+
+def checkIfFileExists(lstName):
+    if os.path.exists(lstName + ".txt"):
+        return True
+    return False
+
+def main():
+    print(createList("toDo"))
+    print(addToList("toDo", "get george from football"))
+    print(addToList("toDo", "rev up that Bugattii weea"))
+    print(removeFromList("toDo", "rev up that Bugattii weea"))
+    print(readList("toDo"))
+    print(deleteList("hello"))
+    print(clearList("hello"))
+    print(deleteList("toDo"))
+
+main()
