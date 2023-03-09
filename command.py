18942d52afec0a9b486a9541f9fa9c18177a9b51
Kaanan Kharwa <kaanan99@gmail.com>
2023-03-09 10:58:44 -0800

    Added highs and lows

--- command.py
+++ command.py
@@ -3,13 +3,11 @@ import weather
 
 def get_city(pos):
     city_name = []
-    flag = False
     for word, gram in pos:
-        if word == "weather":
-            flag = True
-        if flag and gram == "NNP":
+        if gram == "NNP":
             city_name.append(word)
     return " ".join(city_name)
+
 def run_command(text):
     tokens = nltk.word_tokenize(text)
     pos = nltk.pos_tag(tokens)
@@ -23,10 +21,23 @@ def run_command(text):
             elif "kelvin" in tokens or "kelvin" in tokens:
                 return weather.get_current_weather(city, f=False, c=False)
             return weather.get_current_weather(city)
+    elif ("highs" in tokens and "lows" in tokens) or ("high" in tokens and "low" in tokens):
+        city = get_city(pos)
+        if len(city) == 0:
+            return "Please specify a city"
+        else:
+            if "celsius" in tokens or "Celsius" in tokens:
+                return weather.get_high_low(city, f=False, c=True)
+            elif "kelvin" in tokens or "kelvin" in tokens:
+                return weather.get_high_low(city, f=False, c=False)
+            return weather.get_high_low(city)
     else:
         return "I cannot understand your command"
+
+
+
 def main():
-    text = "What is the weather in San Luis Obispo in celsius"
+    text = "What are the highs and lows in San Luis Obispo"
     result_text = run_command(text)
     print(result_text)
 
